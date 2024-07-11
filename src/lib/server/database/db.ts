6fa12c9e0f650d98a246469fd7619b4bc2205d04
Nick Potter <nickpotter1719@gmail.com>
2024-07-10 22:52:38 -0600

    updated env imports

--- src/lib/server/database/db.ts
+++ src/lib/server/database/db.ts
@@ -4,17 +4,17 @@ import { createClient } from '@libsql/client';
 import { env } from '$env/dynamic/private';
 import * as schema from '$lib/server/database/schema'
 
-// modified to use TURSO_DB_URL for local development
+// modified to use TURSO_DB_URL for local
 // const url = dev ? 'file:local.db' : env.TURSO_DB_URL;
 
-const url = import.meta.env.TURSO_DB_URL?.trim();
+const url = env.TURSO_DB_URL;
 
 if (!url) {
 	throw new Error('TURSO_DB_URL is not set');
 }
-if (!dev && !import.meta.env.TURSO_DB_AUTH_TOKEN?.trim()) {
+if (!dev && !env.TURSO_DB_AUTH_TOKEN) {
 	throw new Error('TURSO_DB_AUTH_TOKEN is not set');
 }
 
-const libsql = createClient({ url, authToken: import.meta.env.TURSO_DB_AUTH_TOKEN?.trim() });
+const libsql = createClient({ url, authToken: env.TURSO_DB_AUTH_TOKEN });
 export const db = drizzle(libsql, { schema });
